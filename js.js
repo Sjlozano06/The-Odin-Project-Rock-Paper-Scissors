040b3d4608c9cf34e901aa907d0af71fa1ba90c3
Steven <sjlozano06@gmail.com>
2022-08-15 19:14:27 -0400

    Finished creating UI. Added events that show when cursor hovers over button, when computer makes a selection and updates scoreboard.

--- js.js
+++ js.js
@@ -1,117 +1,141 @@
-/* FUNCTIONS */
+const buttonRock = document.querySelector('#rock');
+const buttonPaper = document.querySelector('#paper');
+const buttonScissors = document.querySelector('#scissors');
+const pScore = document.querySelector('#pScore');
+const cScore = document.querySelector('#cScore');
 
-            /* This function gets the user input and converts the entire string to lower case */
+/* Creating UI effects when cursor hovers over a button */
 
-            function getPlayerSelection (){
-                while (true){
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
 
-                let playerSelection = prompt('Please enter one of the following: Rock, Paper, or Scissors');
-                playerSelection = playerSelection.toLowerCase();
-                
-                if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors'){
-                    return playerSelection;
-                }
+    button.addEventListener('mouseover',() => {
+        button.classList.toggle('mouseOverButton');
+    })
 
-                else {
-                    console.log('Not a valid prompt. Please try again!');
-                }
+    button.addEventListener('mouseout',() => {
+        button.classList.toggle('mouseOverButton');
+     })
+
+});
+
+
+/* Generates a random number from 1-3, and depending on that number, assigns the computer selection to rock, paper, or scissors */
+
+function getComputerSelection(){
+    min = 1;
+    max = 3;
+    computerSelection = Math.floor(Math.random()*(max-min+1)+min);
+    
+    if (computerSelection == 1){
+        computerSelection = 'rock';
+        buttonRock.classList.toggle('computerSelecting');
+    }
+    
+    else if (computerSelection == 2){
+        computerSelection = 'paper';
+        buttonPaper.classList.toggle('computerSelecting');
+    }
+    else {
+        computerSelection = 'scissors';
+        buttonScissors.classList.toggle('computerSelecting');
+    }
+    return computerSelection;
+}
+
+/* Gets both input from the user and the computer, announces the inputs, determines who wins, and keeps score. */
+
+function announceResults(playerSelection,computerSelection){
+
+    console.log('You chose: ', playerSelection);
+    console.log('The computer chose: ', computerSelection);
+    
+    if (playerSelection == 'rock' && computerSelection == 'paper'){
+        console.log('The computer won! Paper beats rock!');
+        computerScore++;
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'scissors'){
+        console.log('You won! rock beats scissors!');
+        userScore++;
+    }
+    
+    else if (playerSelection == 'paper' && computerSelection == 'scissors'){
+        console.log('The computer won! scissors beats paper!');
+        computerScore++;
+    }
+    
+    else if (playerSelection == 'paper' && computerSelection == 'rock'){
+        console.log('You won! paper beats rock!');
+        userScore++;
+    }
+    
+
+    else if (playerSelection == 'scissors' && computerSelection == 'rock'){
+        console.log('The computer won! rock beats scissors!');
+        computerScore++;
+    }
+    
+    else if (playerSelection == 'scissors' && computerSelection == 'paper'){
+        console.log('You won! scissors beats paper!');
+        userScore++;
+    }
+    
+    else {
+        console.log(`It's a tie!`);
+    }
+    
+    let userScoreString = userScore.toString();
+    let computerScoreString = computerScore.toString();
+    
+    pScore.textContent = userScoreString;
+    cScore.textContent = computerScoreString;
+    console.log('The User score is:',userScore,'and the computer score is:',computerScore);
+}
+
+function clearButtons(computerSelection){
+
+    if (computerSelection == 'rock'){
+        buttonRock.classList.toggle('computerSelecting');
+    }
+    else if (computerSelection == 'paper'){
+        buttonPaper.classList.toggle('computerSelecting');
+    }
+    else {
+        buttonScissors.classList.toggle('computerSelecting');
+    }
+}
+
+
+function playRound() {
+    buttons.forEach((button) => {
+        button.addEventListener('click',() => {
+            
+            let playerSelection;
+            if (button == buttonRock){
+                playerSelection = 'rock'
             }
+            else if (button == buttonPaper){
+                playerSelection = 'paper'
+    
+            }
+            else {
+                playerSelection = 'scissors'
             }
 
-            /* Generates a random number from 1-3, and depending on that number, assigns the computer selection to rock, paper, or scissors */
+            getComputerSelection();
+            announceResults(playerSelection,computerSelection);
+            setTimeout(clearButtons,500,computerSelection);
 
-            function getComputerSelection(){
-                min = 1;
-                max = 3;
-                computerSelection = Math.floor(Math.random()*(max-min+1)+min);
-                
-                if (computerSelection == 1){
-                    computerSelection = 'rock';
-                }
 
-                else if (computerSelection == 2){
-                    computerSelection = 'paper';
-                }
-                else {
-                    computerSelection = 'scissors';
-                }
-                return computerSelection;
-            }
 
-            /* Gets both input from the user and the computer, announces the inputs, determines who wins, and keeps score. */
+        })
+    });
+}
 
-            function announceResults(playerSelection,computerSelection){
-               
-                console.log('You chose: ', playerSelection);
-                console.log('The computer chose: ', computerSelection);
+let userScore = 0;
+let computerScore = 0;
+pScore.textContent = '0';
+cScore.textContent = '0';
+playRound();
 
-                if (playerSelection == 'rock' && computerSelection == 'paper'){
-                    console.log('The computer won! Paper beats rock!');
-                    computerScore++;
-                }
-                else if (playerSelection == 'rock' && computerSelection == 'scissors'){
-                    console.log('You won! rock beats scissors!');
-                    userScore++;
-                }
-                
-                else if (playerSelection == 'paper' && computerSelection == 'scissors'){
-                    console.log('The computer won! scissors beats paper!');
-                    computerScore++;
-                }
 
-                else if (playerSelection == 'paper' && computerSelection == 'rock'){
-                    console.log('You won! paper beats rock!');
-                    userScore++;
-                }
-                
-
-                else if (playerSelection == 'scissors' && computerSelection == 'rock'){
-                    console.log('The computer won! rock beats scissors!');
-                    computerScore++;
-                }
-                
-                else if (playerSelection == 'scissors' && computerSelection == 'paper'){
-                    console.log('You won! scissors beats paper!');
-                    userScore++;
-                }
-
-                else {
-                    console.log(`It's a tie!`);
-                }
-
-                console.log('The User score is:',userScore,'and the computer score is:',computerScore);
-            }
-
-            /* Combines the last three functions together into a single function. This encompasses a single round */
-
-            function playRound(){
-
-                playerSelection = getPlayerSelection();
-                computerSelection = getComputerSelection()
-                announceResults(playerSelection,computerSelection);
-
-            }
-
-            /* Utlizes all other functions to play five rounds and determines who wins overall */
-
-            function game(){
-                
-            for (let i = 0; i<5; i++){
-                playRound();
-            }
-            if (userScore > computerScore){
-                console.log('You won the game!');
-            }
-            else if (userScore < computerScore){
-                console.log('You lost the game!');
-            }
-            else{
-                console.log('It is a tie!');
-            }
-            }
-
-            /* END OF FUNCTIONS */
-
-            let userScore = 0;
-            let computerScore = 0;
-            game();
